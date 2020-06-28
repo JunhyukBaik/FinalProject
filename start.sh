9abcb27d798f98a4fd9db1f98623a095f1da5674
sangsang1711 <62577478+sangsang1711@users.noreply.github.com>
2020-06-29 02:31:24 +0900

    start.sh file renew

---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir templates
+mkdir upload
+
+pip3 install Flask
+pip3 install requests
+pip3 install bs4
+pip3 install numpy
+pip install -U Werkzeug
+
+if [ -f "URLresult.html" ]; then
+	mv URLresult.html templates
+fi
+
+if [ -f "cospop.html" ]; then
+	mv cospop.html templates
+fi
+
+if [ -f "tfpop.html" ]; then
+	mv tfpop.html templates
+fi
+
+if [ -f "OSP_finhome.html" ]; then
+	mv OSP_finhome.html templates
+fi
+
+
+echo Starting...
+flask run
+
